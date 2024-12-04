ffbd9fe7f9066c5f1cd48c64d81e80fdb72bd417
smyrzash <sanzhar_myrzash@mail.ru>
2024-12-04 21:15:24 +0500

    validation

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module go_web
 
 go 1.22
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
+	github.com/gorilla/mux v1.8.1
+)
+
+require github.com/asaskevich/govalidator v0.0.0-20200108200545-475eaeb16496 // indirect
--- part2/main.go
+++ part2/main.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	is "github.com/go-ozzo/ozzo-validation/v4/is"
 )
 
 type Result struct {
@@ -14,8 +17,21 @@ type Result struct {
 
 type User struct {
 	Id       uint   `json:"id"`
+	FullName string `json:"full_name"`
 	Grade    string `json:"grade"`
 	Language string `json:"lang"`
+	Email    string `json:"email"`
+	Phone    string `json:"phone"`
+}
+
+func (u User) Validate() error {
+	return validation.ValidateStruct(&u,
+		validation.Field(&u.Id, validation.Required),
+		validation.Field(&u.FullName, validation.Required, is.Alpha),
+		validation.Field(&u.Grade, validation.Required, validation.NotNil, is.Alpha),
+		validation.Field(&u.Email, validation.Required, validation.NotNil, is.Email),
+		validation.Field(&u.Phone, validation.Required, is.E164),
+	)
 }
 
 func main() {
@@ -58,11 +74,21 @@ func UserHandler(w http.ResponseWriter, r *http.Request) {
 	var user User
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		fmt.Println("ERROR:", err.Error())
+		fmt.Println("Decoding Error:", err.Error())
 		WriteJson(w, http.StatusInternalServerError, map[string]interface{}{
 			"ok":    false,
 			"error": "Internal Server Error",
 		})
+		return
+	}
+	err = user.Validate()
+	if err != nil {
+		WriteJson(w, http.StatusBadRequest, map[string]interface{}{
+			"ok":    false,
+			"error": "Bad Request",
+		})
+		fmt.Fprintln(w, "Validation Error:", err.Error())
+		fmt.Println("Validation Error:", err.Error())
 		return
 	}
 	fmt.Printf("data from request %v\n", user)
