0c92a88a1e3810ffbcb4155e1e2ea09f2c497fc1
smyrzash <sanzhar_myrzash@mail.ru>
2024-12-04 16:49:23 +0500

    json response

---
+++ part2/json.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+var Data = `
+	{
+		"id":1,
+		"age":19,
+		"badge": [
+			{
+				"leetcode": "yes",
+				 "db" : "yes"
+			}, 
+			{
+				"leetcode": "no", 
+				"db" : "yes"
+			}
+		]
+	}
+`
+
+type DataStruct struct {
+	Id    uint    `json:"id"`
+	Age   uint    `json:"age"`
+	Badge []Badge `json:"badge"`
+	Brad  bool
+}
+
+type Badge struct {
+	Leetcode string `json:"leetcode"`
+	Db       string `json:"db"`
+}
+
+type User struct {
+	Id   int    `json:"identification"`
+	Name string `json:"username"`
+}
+
+func mainer() {
+	user1 := User{10, "Sanzhar"}
+	output, _ := json.MarshalIndent(user1, "", "    ")
+	fmt.Println(string(output))
+
+	var data DataStruct
+	err := json.Unmarshal([]byte(Data), &data)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(data)
+}
--- part2/main.go
+++ part2/main.go
@@ -3,51 +3,38 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 )
 
-var Data = `
-	{
-		"id":1,
-		"age":19,
-		"badge": [
-			{
-				"leetcode": "yes",
-				 "db" : "yes"
-			}, 
-			{
-				"leetcode": "no", 
-				"db" : "yes"
-			}
-		]
-	}
-`
-
-type DataStruct struct {
-	Id    uint    `json:"id"`
-	Age   uint    `json:"age"`
-	Badge []Badge `json:"badge"`
-	Brad  bool
-}
-
-type Badge struct {
-	Leetcode string `json:"leetcode"`
-	Db       string `json:"db"`
-}
-
-type User struct {
-	Id   int    `json:"identification"`
-	Name string `json:"username"`
+type Result struct {
+	Id       uint   `json:"identificator"`
+	Age      uint   `json:"age"`
+	FullName string `json:"full_name"`
 }
 
 func main() {
-	user1 := User{10, "Sanzhar"}
-	output, _ := json.MarshalIndent(user1, "", "    ")
-	fmt.Println(string(output))
-
-	var data DataStruct
-	err := json.Unmarshal([]byte(Data), &data)
+	http.HandleFunc("/json", jsonResult)
+	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(data)
+}
+
+func jsonResult(w http.ResponseWriter, r *http.Request) {
+	result := Result{
+		1, 19, "Sanzhar Myrzash",
+	}
+	response, err := json.Marshal(result)
+	// err = errors.New("test error")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		resultError := map[string]any{
+			"ok":           false,
+			"errorMessage": err.Error(),
+		}
+		json.NewEncoder(w).Encode(resultError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w, string(response))
 }
